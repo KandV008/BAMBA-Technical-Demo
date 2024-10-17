f8cdb5b11e9c695c7652ce458838fe8fd36102b4
Krokodile <117300490+KandV008@users.noreply.github.com>
2024-10-17 15:19:33 +0200

    feat: Add Map Component

---
+++ components/map.tsx
@@ -0,0 +1,12 @@
+import { View } from "react-native";
+import { styled } from "nativewind";
+
+const StyledView = styled(View);
+
+export default function Map() {
+  return (
+    <StyledView className="w-full h-full bg-green-200">
+      
+    </StyledView>
+  );
+}
