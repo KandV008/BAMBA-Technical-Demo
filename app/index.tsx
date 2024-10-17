6cda9669e894f76d0532a332888c4ca7618d6468
Krokodile <117300490+KandV008@users.noreply.github.com>
2024-10-17 09:26:46 +0200

    refactor: Restructure project

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import Map from "@/components/map";
+import NavigationBar from "@/components/navigationBar";
+import Options from "@/components/options";
+import SearchBar from "@/components/searchBar";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+    </View>
+  );
+}
