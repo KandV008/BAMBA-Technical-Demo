8002cc8e39c28fcad4726545f2149cac6dc3eaee
Krokodile <117300490+KandV008@users.noreply.github.com>
2024-10-30 19:54:53 +0100

    feat: Add Map View

--- components/map.tsx
+++ components/map.tsx
@@ -1,12 +1,23 @@
-import { View } from "react-native";
+import { StyleSheet, View } from 'react-native';
 import { styled } from "nativewind";
+import MapView from 'react-native-maps';
 
 const StyledView = styled(View);
 
 export default function Map() {
   return (
     <StyledView className="w-full h-full bg-green-200">
-      {/* TODO Add MapView */}
+      <MapView style={styles.map} />
     </StyledView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
